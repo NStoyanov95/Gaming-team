9443b43c8fe8bfe41bbb85c1fdd301c74054a417
NStoyanov95 <nstoyanov9512@gmail.com>
2024-02-16 21:04:25 +0200

    Add games controller

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,11 @@ const router = require('express').Router();
 
 const homeController = require('./controllers/homeController');
 const authController = require('./controllers/authController');
+const gamesController = require('./controllers/gamesController');
 
 router.use(homeController);
 router.use('/auth', authController);
-
+router.use('/games', gamesController);
 
 
 
