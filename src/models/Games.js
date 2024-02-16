91e4702b7d347876ec0ed9931279f673862f8743
NStoyanov95 <nstoyanov9512@gmail.com>
2024-02-16 20:58:06 +0200

    Create games model

---
+++ src/models/Games.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const gamesSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+    },
+    image:{
+        type: String,
+        required: true,
+    },
+    price:{
+        type: Number,
+        required: true,
+    },
+    description:{
+        type: String,
+        required: true,
+    },
+    genre:{
+        type: String,
+        required: true,
+    },
+    platform:{
+        type: String,
+        required: true,
+        enum: ['PC', 'Nintendo', 'PS4', 'PS5', 'XBOX']
+    },
+    boughtBy: [{
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    }],
+    owner: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    }
+})
