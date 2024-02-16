960c434ad905533821f18170dbed8bcee6cd490c
NStoyanov95 <nstoyanov9512@gmail.com>
2024-02-16 21:08:30 +0200

    Show create page

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -1,5 +1,21 @@
 const router = require('express').Router();
 
+const { getErrorMessage } = require('../utils/errorUtils');
+
+router.get('/create', (req, res) => {
+    res.render('games/create');
+});
+
+router.post('/create', async (req, res) => {
+    const gameData = req.body;
+
+    try {
+        await gamesService.create(gameData);
+        res.redirect('/')
+    } catch (error) {
+        res.render('games/create', { error: getErrorMessage(error) })
+    }
+})
 
 
 
