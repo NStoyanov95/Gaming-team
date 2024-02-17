3dad62895afab9912c23578f22e19856616a5324
NStoyanov95 <nstoyanov9512@gmail.com>
2024-02-17 13:00:19 +0200

    Add game model validation

--- src/models/Game.js
+++ src/models/Game.js
@@ -3,28 +3,38 @@ const mongoose = require('mongoose');
 const gamesSchema = new mongoose.Schema({
     name:{
         type: String,
-        required: true,
+        required: [true, 'Please enter name'],
+        minLength: [4, 'Name should be at least 4 characters long']
     },
     image:{
         type: String,
-        required: true,
+        required: [true, 'Please enter image address'],
+        match: [/^https?:\/\//, 'Image URL should start with http://" or "https://']
     },
     price:{
         type: Number,
-        required: true,
+        required: [true, 'Please enter price'],
+        min: [0, 'Price should be positive number']
     },
     description:{
         type: String,
-        required: true,
+        required: [true, 'Please enter description'],
+        minLength: [10, 'Description should be at least 10 characters long']
     },
     genre:{
         type: String,
-        required: true,
+        required: [true, 'Please enter genre'],
+        minLength: [2, 'Genre should be at least 2 characters long']
+
     },
     platform:{
         type: String,
-        required: true,
-        enum: ['PC', 'Nintendo', 'PS4', 'PS5', 'XBOX']
+        required: [true, 'Please enter platform'],
+        enum:{
+           values: ['PC', 'Nintendo', 'PS4', 'PS5', 'XBOX'],
+           message: 'Platform must be one of the following options: "PC", "Nintendo", "PS4", "PS5", "XBOX"'
+        } 
+            
     },
     boughtBy: [{
         type: mongoose.Types.ObjectId,
